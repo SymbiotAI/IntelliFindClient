d57b03f09cbabc664c93811baaa7d9bbcf9bf124
akhotenko <megamufio@gmail.com>
2023-06-11 21:42:04 +0100

    seaarch text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observer } from 'rxjs';
 import axios from "axios";
+import { IonTextarea } from '@ionic/angular';
+import { S3ServiceService } from './s3-service.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,25 +12,54 @@ import axios from "axios";
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private s3Service: S3ServiceService) {
+  }
+  isToastOpen = false;
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
+  results: { page_content: string } [] = [];
+  @ViewChild('searchText') searchText!: IonTextarea;
+  async send() {
+    const textValue = this.searchText.value;
+    console.log('Text value:', textValue);
+    await axios.get('http://ec2-3-122-229-15.eu-central-1.compute.amazonaws.com/search?text=' + textValue)
+    .then((resp) => {
+      console.log(resp)
+      this.results = resp.data;
+      console.log(this.results)
+    })
+    .catch((resp) => {console.error(resp)})
+    // Do something with the text value
   }
   async loadImageFromDevice(event: any) {
-    console.log(event.target)
+    // console.log(event.target)
+    // this.isToastOpen = true;
+    
+    // const files: FileList = event.target.files;
+    // const formData = new FormData();
 
-    const files: FileList = event.target.files;
-    const formData = new FormData();
+    // for (let i = 0; i < files.length; i++) {
+    // const file: File | null = files.item(i);
 
-    for (let i = 0; i < files.length; i++) {
-    const file: File | null = files.item(i);
-
-    if (file) {
-      console.log("file", file)
-      formData.append('files', file);
-    }
+    // if (file) {
+    //   console.log("file", file)
+    //   formData.append('files', file);
+    // }
+    this.s3Service.uploadFile(event.target.files[0]);
   }
 
-  await axios.post('http://ec2-3-122-229-15.eu-central-1.compute.amazonaws.com/', formData)
-  }
+
+  //
+
+
+    // await axios.post('http://ec2-3-122-229-15.eu-central-1.compute.amazonaws.com/', formData)
+    // .then((resp) => {console.log(resp)})
+    // .catch((resp) => {console.error(resp)})
+   
 }
+
 
 
